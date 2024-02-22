10f1de291d18499a2a723879ceda890cfb352d32
Ryan Adoremos <dzy5jk@virginia.edu>
2024-02-21 22:11:42 -0500

    Added navigation and started work on SQLite

--- app/src/main/java/com/ryanadoremos/bucketlist/AddActivity.kt
+++ app/src/main/java/com/ryanadoremos/bucketlist/AddActivity.kt
@@ -1,11 +1,26 @@
 package com.ryanadoremos.bucketlist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        val saveBtn: Button = findViewById(R.id.button)
+        saveBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val cancelBtn: Button = findViewById(R.id.button2)
+        cancelBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
--- app/src/main/java/com/ryanadoremos/bucketlist/MainActivity.kt
+++ app/src/main/java/com/ryanadoremos/bucketlist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ryanadoremos.bucketlist
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,8 +11,37 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.ryanadoremos.bucketlist.databinding.ActivityMainBinding
+import android.widget.Button
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.content.ContentValues
+import android.content.Context
 
-class MainActivity : AppCompatActivity() {
+class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, "test", factory, 1) {
+    // below is the method for creating a database by a sqlite query
+    override fun onCreate(db: SQLiteDatabase) {
+        // below is a sqlite query, where column names
+        // along with their data types is given
+        val query = ("CREATE TABLE " + "BucketList" + " ("
+                + "Key" + " INTEGER PRIMARY KEY NOT NULL AUTO, " +
+                "ItemName" + " TEXT NOT NULL," +
+                "DueDate" + " DATE NOT NULL," +
+                "Complete" + " BOOLEAN NOT NULL," +
+                "ComDate" + "DATE NULL" +")")
+        // we are calling sqlite
+        // method for executing our query
+        db.execSQL(query)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        // this method is to check if table already exists
+        db.execSQL("DROP TABLE IF EXISTS " + "BucketList")
+        onCreate(db)
+    }
+}
+    class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -28,9 +58,16 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        //binding.fab.setOnClickListener { view ->
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //.setAction("Action", null).show()
+        //
+
+       // }
+        val addBtn: Button = findViewById(R.id.button3)
+        addBtn.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
         }
     }
 
